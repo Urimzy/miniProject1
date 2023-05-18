a4727309d8539cec4f109066f6fb48fc6756804f
Urimzy <100048070+Urimzy@users.noreply.github.com>
2023-05-18 11:10:59 +0200

    Add files via upload

---
+++ Miniprojekt1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class ArrayMin
+{
+    public int Min(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            throw new ArgumentException("Array eshte i zbrazet ose 0");
+        }
+
+        int minValue = array[0];
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < minValue)
+            {
+                minValue = array[i];
+            }
+        }
+
+        return minValue;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Vendos se sa elemente deshiron te permbaj Array: ");
+        int length = int.Parse(Console.ReadLine());
+
+        int[] array = new int[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write($"Sheno Elementin {i + 1}: ");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        ArrayMin arrayMin = new ArrayMin();
+        int min = arrayMin.Min(array);
+
+        Console.WriteLine($"Elementi me i vogel ne Array eshte: {min}");
+    }
+}
---
+++ Miniprojekt2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+class Employee
+{
+    public int Id { get; set; }
+    public string Emri { get; set; }
+    public string Pozita { get; set; }
+    public decimal Rroga { get; set; }
+
+    public virtual decimal CalculateBonus(decimal rroga)
+    {
+        return 0;
+    }
+}
+
+class Zhvillues : Employee
+{
+    public override decimal CalculateBonus(decimal rroga)
+    {
+        decimal bonus = Math.Max(50000, rroga * 0.2m);
+        return bonus;
+    }
+}
+
+class Menaxher : Employee
+{
+    public override decimal CalculateBonus(decimal rroga)
+    {
+        decimal bonus = Math.Max(50000, rroga * 0.25m);
+        return bonus;
+    }
+}
+
+class Admin : Employee
+{
+    public override decimal CalculateBonus(decimal rroga)
+    {
+        return 50000;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the type of worker (Zhvillues, Menaxher, or Admin): ");
+        string workerType = Console.ReadLine();
+
+        Employee employee;
+
+        switch (workerType.ToLower())
+        {
+            case "zhvillues":
+                employee = new Zhvillues();
+                break;
+            case "menaxher":
+                employee = new Menaxher();
+                break;
+            case "admin":
+                employee = new Admin();
+                break;
+            default:
+                Console.WriteLine("Invalid worker type!");
+                return;
+        }
+
+        Console.WriteLine("Enter employee details:");
+        Console.Write("Id: ");
+        employee.Id = int.Parse(Console.ReadLine());
+        Console.Write("Emri: ");
+        employee.Emri = Console.ReadLine();
+        Console.Write("Pozita: ");
+        employee.Pozita = Console.ReadLine();
+        Console.Write("Rroga: ");
+        employee.Rroga = decimal.Parse(Console.ReadLine());
+
+        decimal bonus = employee.CalculateBonus(employee.Rroga);
+
+        Console.WriteLine("Employee details:");
+        Console.WriteLine($"Id: {employee.Id}");
+        Console.WriteLine($"Emri: {employee.Emri}");
+        Console.WriteLine($"Pozita: {employee.Pozita}");
+        Console.WriteLine($"Rroga: {employee.Rroga}");
+        Console.WriteLine($"Bonus: {bonus}");
+    }
+}
